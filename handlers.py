7ff1686190c0af5a0df69b1f71efb3a77ef2c66c
Scar-Proger <antonkarpovic8@gmail.com>
2026-01-31 18:52:57 +0300

    Описание изменений

--- handlers.py
+++ handlers.py
@@ -328,7 +328,7 @@ async def show_menu(bot: Bot, chat_id: int, message_id: int = None):
     builder.row(
         InlineKeyboardButton(
             text=t(user, "btn_support"),
-            url="mailto:megashix.vpn@mail.ru"
+            url="https://t.me/cold_snake"
         )
     )
 

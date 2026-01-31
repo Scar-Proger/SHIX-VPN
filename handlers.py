663aa779374ceef946dffa85004ebe3dbc3fa866
Scar-Proger <antonkarpovic8@gmail.com>
2026-01-31 19:46:10 +0300

    Описание изменений

--- handlers.py
+++ handlers.py
@@ -328,7 +328,7 @@ async def show_menu(bot: Bot, chat_id: int, message_id: int = None):
     builder.row(
         InlineKeyboardButton(
             text=t(user, "btn_support"),
-            url="https://clc.li/MrgdY"
+            url="https://t.me/shix_vpn"
         )
     )
 

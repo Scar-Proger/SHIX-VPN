eb7cc4e87cbe14bcac6379ee7c27a9474e03785e
Антон Карпович <124394587+Scar-Proger@users.noreply.github.com>
2026-03-20 00:43:32 +0300

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -1036,27 +1036,27 @@ async def check_payment(callback: CallbackQuery):
     # ТЕКСТ ДЛЯ ПОЛЬЗОВАТЕЛЯ
     # =========================================================
     if result == "CONFIRMED":
-         caption = t(user, "payment_success")
+        caption = t(user, "payment_success")
 
-         username = callback.from_user.username
-         user_id = callback.from_user.id
+        username = callback.from_user.username
+        user_id = callback.from_user.id
 
-         if username:
+        if username:
             support_text = (
                 "💰 Новая оплата\n\n"
                 f"👤 Username: @{username}\n"
                 f"🆔 ID: {user_id}\n"
                 f"💳 TX: {tx_id}"
             )
-         else:
+        else:
             support_text = (
                 "💰 Новая оплата\n\n"
                 f"👤 ID: {user_id}\n"
                 f"💳 TX: {tx_id}"
             )
 
-         kb = None
-         if username:
+        kb = None
+        if username:
             kb = InlineKeyboardMarkup(
                 inline_keyboard=[
                     [

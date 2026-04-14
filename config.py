bb2980037aa5004d219df0981d142511859e3ad9
Scar-Proger <antonkarpovic8@gmail.com>
2026-04-14 21:42:15 +0300

    Обновление кода

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config(BaseModel):
 
     PLATEGA_BASE_URL: str = os.getenv("PLATEGA_BASE_URL", "https://app.platega.io")
     MERCHANT_ID: str = os.getenv("MERCHANT_ID","6120e89a-93dc-4359-a578-8eed028e8c1f")
-    SECRET_KEY: str = os.getenv("SECRET_KEYMERCHANT_ID", "FSfD28jrJxiKZlbaqp5e4EgD8QI3ClHgV1uvh8FHZ3qxlOXo46AaOoNG3oJqWFqdgDDgmDbTFJ8FuY09SMeSQw6MdUQUe6HFX0Gn")
+    SECRET_KEY: str = os.getenv("SECRET_KEYMERCHANT_ID", "tNnsTqOOUxREKZIsDgyRNJnuOsdjKVIqSCjbbhHI04i6DRnro3iCsL1AZNN5gPrHe3yuHIqNZCFHZ4nRpPBYAsidkZUWQQNNilLQ")
 
     REMNAWAVE_API_URL: str = os.getenv("REMNAWAVE_API_URL", "https://panel.shix-vpn.space/api")
     REMNAWAVE_API_KEY: str = os.getenv("REMNAWAVE_API_KEY", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiMTljZjA2MzAtNjlmZC00MjQyLWIxNGMtZmI0YTlmNWU5MDYzIiwidXNlcm5hbWUiOm51bGwsInJvbGUiOiJBUEkiLCJpYXQiOjE3NjkxMTk5NjIsImV4cCI6MTA0MDkwMzM1NjJ9.Q800pLD7zQfFlHEKPzMXEzPXg6W2QXm2LgyRgb_dOQU")
